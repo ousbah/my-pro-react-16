1ca93e0fd9f28755f4119ec41bce6d0176e860df
ousbah <oussbahh@gmail.com>
2020-09-25 14:53:27 +0200

    Understanding Bootstrap - Creating grids

--- 03-HTML-JSX-and-CSS-Primer/primer/src/App.js
+++ 03-HTML-JSX-and-CSS-Primer/primer/src/App.js
@@ -13,23 +13,37 @@ export default class App extends Component {
     return val % 2 === 0 ? 'Even' : 'Odd'
   }
 
-  getClassName(val) {
-    return val % 2 === 0
-        ? "bg-primary text-white text-center p-2 m-1"
-        : "bg-secondary text-white text-center p-2 m-1"
-  }
-
   handleClick = () => {
     this.setState({ count: this.state.count + 1 })
   }
 
   render = () => (
-      <h4 className={ this.getClassName(this.state.count) }>
-        <button className="btn btn-info m-2" onClick={ this.handleClick }>
-          Click Me
-        </button>
-        Number of things: { this.isEven(this.state.count) }
-      </h4>
+      <div className="container-fluid p-4">
+        <div className="row bg-info text-white p-2">
+          <div className="col font-weight-bold">
+            Value
+          </div>
+          <div className="col-6 font-weight-bold">
+            Even?
+          </div>
+        </div>
+        <div className="row bg-light p-2 border">
+          <div className="col">
+            { this.state.count }
+          </div>
+          <div className="col-6">
+            { this.isEven(this.state.count) }
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <button className="btn btn-info m-2" 
+                    onClick={ this.handleClick }>
+              Click Me
+            </button>
+          </div>
+        </div>
+      </div>
   )
 }
 
