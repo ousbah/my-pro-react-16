a68da0ba236d3a6dfc16c360e24568314832be57
ousbah <oussbahh@gmail.com>
2020-09-25 16:28:48 +0200

    Understanding Bootstrap - Styling forms.

--- 03-HTML-JSX-and-CSS-Primer/primer/src/App.js
+++ 03-HTML-JSX-and-CSS-Primer/primer/src/App.js
@@ -2,46 +2,17 @@ import React, { Component } from 'react'
 
 export default class App extends Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      count: 4
-    }
-  }
-
-  isEven(val) {
-    return val % 2 === 0 ? 'Even' : 'Odd'
-  }
-
-  handleClick = () => {
-    this.setState({ count: this.state.count + 1 })
-  }
-
   render = () => (
-    <table className="table table-striped table-bordered table-sm">
-      <thead className="bg-info text-white">
-        <tr>
-          <th>Value</th>
-          <th>Even?</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>{ this.state.count }</td>
-          <td>{ this.isEven(this.state.count) }</td>
-        </tr>
-      </tbody>
-      <tfoot className="text-center">
-        <tr>
-          <td colSpan="2">
-            <button className="btn btn-info m-2" 
-                    onClick={ this.handleClick }>
-              Click Me
-            </button>
-          </td>
-        </tr>
-      </tfoot>
-    </table>
+      <div className="m-2">
+        <div className="form-group">
+          <label>Name:</label>
+          <input className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>City:</label>
+          <input className="form-control" />
+        </div>
+      </div>
   )
 }
 
